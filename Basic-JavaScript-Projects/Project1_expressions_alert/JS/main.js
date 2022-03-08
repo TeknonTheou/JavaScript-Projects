df892c07ad6da5cc29b737b780e01aaede05cdfe
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-03-08 13:58:18 -0600

    Update JavaScript projects

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,9 +1,9 @@
 var A="Don\'t you love my demo of the \"Window.Alert\" method from a variable?"; //defines variable A
 window.alert(A);//displays variable A in new window
 var B="Document.write from a variable.\n";//defines variable B
-document.write(B);//displays variable B in document
-var C="I know some basic JavaScript,"+"so this is pretty easy so far.\n";//concatenates a string and defines it as Variable C
-document.write(C);//displays variable C in document
+document.write(B+"<br>");//displays variable B in document
+var C="I know some basic JavaScript, "+"so this is pretty easy so far.\n";//concatenates a string and defines it as Variable C
+document.write(C+"<br>");//displays variable C in document
 var Transportation = "Family Car", Geo="Prism", Dodge="Moby Dimple", Ford="Contour", Mercury="Mystique", Kia="Rhoda"; //defines multiple variables
-document.write(Transportation.fontcolor("red") + ": "+Kia.fontcolor("green")+".\n");//displays variables Transportation and Kia in document with color
-document.write(3*5);//displays result of expression within the parenthesis
+document.write(Transportation.fontcolor("red") + ": "+Kia.fontcolor("green")+"<br>");//displays variables Transportation and Kia in document with color
+document.write((3*5)+"<br>");//displays result of expression within the parenthesis
---
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+var today=new Date();
+var date= (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
+var time = today.getHours()+':'+ today.getMinutes()+':'+today.getSeconds();
+var dateTime=date+'  '+time;
+
+document.getElementById("currentDateTime").value=dateTime;
