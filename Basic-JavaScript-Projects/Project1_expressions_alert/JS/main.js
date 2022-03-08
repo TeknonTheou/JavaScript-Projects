d6bc2bfb99272a4c8e458b0c4f7cf1df9d7fd0bf
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-03-08 12:48:45 -0600

    Create Index.html and main.js

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+var A="Don\'t you love my demo of the \"Window.Alert\" method from a variable?"; //defines variable A
+window.alert(A);//displays variable A in new window
+var B="Document.write from a variable.\n";//defines variable B
+document.write(B);//displays variable B in document
+var C="I know some basic JavaScript,"+"so this is pretty easy so far.\n";//concatenates a string and defines it as Variable C
+document.write(C);//displays variable C in document
+var Transportation = "Family Car", Geo="Prism", Dodge="Moby Dimple", Ford="Contour", Mercury="Mystique", Kia="Rhoda"; //defines multiple variables
+document.write(Transportation.fontcolor("red") + ": "+Kia.fontcolor("green")+".\n");//displays variable Kia in document
+document.write(3*5);//displays result of expression within the parenthesis
