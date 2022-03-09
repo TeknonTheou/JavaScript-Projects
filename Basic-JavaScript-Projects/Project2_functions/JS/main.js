5e247a338ce7d3d5e209a5bbfb517cde05fcbe05
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-03-09 09:34:41 -0600

    push changes to main.js and index.html

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -1,4 +1,9 @@
-function My_First_Function(){
-    var str ="This is the button text!";
-    document.getElementById("Button_Text").innerHTML=str.fontcolor("blue");
+function My_First_Function(){ //This Function changes the text in the button when clicked
+    var str ="This is the button text!";//setting text that will appear inside button when it is clicked.
+    document.getElementById("Button_Text").innerHTML=str.fontcolor("blue");//setting id which will call this function, and changing font to blue.
+}
+function myFunction(){ //This function changes the text of the paragraph element under id "Concatentate" when clicked.
+    var sentence="I am learning"; //setting initial text
+    sentence += " a lot from this book!"; //concatenating text with additional wording
+    document.getElementById("Concatenate").innerHTML=sentence; //setting the id which will call this function
 }
