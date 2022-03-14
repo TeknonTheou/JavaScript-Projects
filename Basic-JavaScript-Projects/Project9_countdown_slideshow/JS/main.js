62466d10406fcdb4213f73f12ddbe6ca2d90387a
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-03-14 11:40:00 -0500

    Submit project9

---
+++ Basic-JavaScript-Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,44 @@
+function countdown(){
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds -1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds==-1){
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML="";
+        }
+    }
+    tick();
+}
+var slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides() {//determines which "dot" is clicked on and starts slide show at that slide
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}//changes which dot is highlighted    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image/dot every 5 seconds
+  }
