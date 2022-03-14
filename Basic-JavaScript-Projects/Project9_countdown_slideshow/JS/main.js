ec302d1b1d1379810cdfd1e1ff3158b3595703ed
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-03-14 11:44:17 -0500

    Update project9

--- Basic-JavaScript-Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic-JavaScript-Projects/Project9_countdown_slideshow/JS/main.js
@@ -16,10 +16,6 @@ function countdown(){
 var slideIndex = 0;
 showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
 // Thumbnail image controls
 function currentSlide(n) {
